406ae41457acbdcf05ddc35b646f20304f32a229
Mia L. Pham <phamml@dukes.jmu.edu>
2022-11-25 16:10:05 -0500

    added filter implementation

--- script.js
+++ script.js
@@ -1,20 +1,61 @@
 const searchBoxElem = document.getElementById("query");
 const searchButton = document.querySelector('.action-search');
 const resultsContainerElem = document.getElementById("results");
+
+const allButton = document.getElementById("all");
+const filterButton1 = document.getElementById("2000");
+const filterButton2 = document.getElementById("2010");
 // console.log(searchBoxElem);
 // console.log(searchButton);
 
 searchButton.addEventListener("click", whenButtonClicked);
+let filterDate;
 
 async function whenButtonClicked(event) {
     console.log("testing");
     console.log(searchBoxElem.value);   
 
     const movies = await searchForMovies(searchBoxElem.value);
+    console.log("movies=");
     console.log(movies);
 
-    const movieResults = await createMovieResults(movies);
+    let movieResults = await createMovieResults(movies);
     console.log(movieResults);
+
+    allButton.addEventListener("click", async (ev) => {
+      console.log("show all");
+      movieResults = await createMovieResults(movies);
+
+      clearResultsElem();
+      populateResultsElem(movieResults);
+    })
+
+    let moviesByDate;
+    filterButton1.addEventListener("click", async (ev) => {
+      console.log("filter 2000");
+      filterDate = 2000;
+      moviesByDate = movies.filter(filterReleaseDate);
+      console.log(moviesByDate);
+
+      movieResults = await createMovieResults(moviesByDate);
+      console.log(movieResults);  
+
+      clearResultsElem();
+      populateResultsElem(movieResults);
+    })
+
+    filterButton2.addEventListener("click", async (ev) => {
+      console.log("filter 2010");
+      filterDate = 2010;
+      moviesByDate = movies.filter(filterReleaseDate);
+      console.log(moviesByDate);
+
+      movieResults = await createMovieResults(moviesByDate);
+      console.log(movieResults); 
+      
+      clearResultsElem();
+      populateResultsElem(movieResults);
+    })
 
     clearResultsElem();
     populateResultsElem(movieResults);
@@ -32,18 +73,35 @@ async function searchForMovies(query) {
   return movieResultsJson.results;
 }
 
+function filterReleaseDate(movie) {
+    console.log(movie.release_date);
+    console.log(filterDate);
+
+    var year = parseInt(movie.release_date);
+    console.log(year);
+
+    console.log(year >= filterDate);
+    return year >= filterDate;
+}
+
 async function createMovieResults(movieResultsJson) {  
-    return movieResultsJson.map((movieTitle, i) => {
+    return movieResultsJson.map((movie, i) => {
       let resultElem = document.createElement("div");
       resultElem.classList.add("result");
       const h3 = document.createElement("h3");
       const h4 = document.createElement("h4");
+      // const img = document.createElement("img");
 
-      h3.append(movieTitle.title);
+      h3.append(movie.title);
       resultElem.append(h3);
-      h4.append("Release Date: " + movieTitle.release_date);
+
+      h4.append("Release Date: " + movie.release_date);
       resultElem.append(h4);
-      resultElem.append(movieTitle.overview);
+
+      resultElem.append(movie.overview);
+
+      // img.src = '/txIt41UgDBJsZ7W33bhXjdqUIv8.jpg';
+      // resultElem.append(img);
       return resultElem;
     });
 }
